2b79a591c894aef262c03f62bfd77a8c93abd0bc
BelenGuillamondegui <belenguilla1@gmail.com>
2023-05-13 12:37:50 -0300

    agrego isologotipo con copyright

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,13 +4,23 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footerContainer">
-      <img className="copyright" src="./copyright.png" alt="imagen copyright" />
-      <div className="footerIcons">
-        <img src="./icon facebook.png" alt="icono Facebook" />
-        <img src="./Vector.png" alt="icono Linkedin" />
-        <img src="./tweet.png" alt="icono Tweeter" />
-        <img src="./insta.png" alt="icono Instagram" />
-      </div>
+        <div className="isologotipo">
+          
+          <img src="./isologotipo.png" alt="isologotipo de Digital Booking" />
+          <img src="./lema.png" alt="lema de Digital Booking" />
+          
+          <img
+            className="copyright"
+            src="./copyright.png"
+            alt="imagen copyright"
+          />
+        </div>
+        <div className="footerIcons">
+          <img src="./icon facebook.png" alt="icono Facebook" />
+          <img src="./Vector.png" alt="icono Linkedin" />
+          <img src="./tweet.png" alt="icono Tweeter" />
+          <img src="./insta.png" alt="icono Instagram" />
+        </div>
       </div>
     </div>
   );
